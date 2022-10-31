67c2735dda63367da99d6fdf5cb86be8568a77a1
Unbrick <Unbrick@users.noreply.github.com>
2022-10-31 17:46:45 +0100

    Update auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -2,81 +2,17 @@ import { NowRequest, NowResponse } from '@vercel/node'
 
 export const allowList: { [iat: string]: AllowListItem } = {
   dtinth: {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzToF2nyjDcJIjsYOPUdi
-karObvmc5XjWYoDvlhNhbQmwr7DhXeoIbxY3+cEzXzcS15bGwuXBJNB9I7q5CAjn
-UBaJPB7zZ1kMlSvJdK8WGFCMfZEqaT5s6+CqMRDuAaaKxluHG9jKNecwAd0iZDJK
-SNZIl0qRljHbzrSPvCLHw5BUO4cI8J5CeCOMDYZXB/4icmVSZSe1ynt8mFjcKhAN
-774MCCB/7uzd81MuQSu0l/gDQpiCW61jaEMsmAsI8X3c4bCiprm5/prefdRB8KLl
-sl1ClPODqzkQAPi+tOYyh01Sx6zzY1lkCQHNLt+jAX20dUd/7aDRcns3bq+l4PLl
-/wIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'journal-web': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtucrzfonc9G+AHu915oO
-h4lgwdJ0zMmREh0EZJKBtzlRw9QKW8mCd9GMKVfv4kYOZur9BcDXElzhBsLM09rh
-7HXV2Ba7h7v+KC2bDl2aoeILye9pERLAnSnUWzJxBRwN9VGFp7rxAzFrHwvA1U3o
-yDsBImg2gQMIOzKWgvvd6HE+pQpxEDz336yzEZDs8186XLeIssbC5jht1lb7s3/0
-yoD83zGow/xLirUG/dS/Fzt1O3xK5rBlNrNxRHwv0t36HCRhhPihG62vV7VwnXnI
-43QyQmtHc0m0LNlo6kvGT+jVzPvMjlHHSFYBVzXz7uLqiC6OpYr9R/UkNkaQzAJw
-1wIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'screenshot-redirector': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuQgPix+cRdWGGfHDumA2
-uFfYMhlv2U0Jxc5ww+vIHeDddocxMOXRhjnWkjZ0z6E6Otq8FRgMKXCRFusoXx81
-nmrXrVwschDGf1ypwtaEN0ee5TQ9BCE68Maw8Haafjbwxw7najWiRk9q3VLQVcwC
-Mau0Zi16q/trasqgjXh5AAKUDO+7vsG1FI4yoNHrcVj+zvuLCPy/dcyJp18HpgoR
-bqMiXzkJJRd4vO7yfkW6NBIaudC+nRxui9isQrTAE9oFum5id01g4rvdEqjdFnYJ
-cZbPdanG0q/KbIQn+yddgujGNrrRmGNO14l+6eSMri+pqR+C3JSRU0VOPbguA+hw
-CwIDAQAB
------END PUBLIC KEY-----`,
-  },
-  automatron: {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA44Ry2SLyTOmnd8lhXRal
-NirZiXgDqZf0xy3V23HpO4tnGYO+OaFCz46BQMGKoEhNT3Hp0f3857xufDTMHL6l
-niZbLZUVHOANV53zfyqt/s1GR+JnUkF+yJ4spNvhRWQVw/Uuwr28/D7VT34FB7Pz
-GMZDyKFziwZAEpiZM95kPzU1BBvKuXsRqzVw51BCsOin7rHSjczK4bpBgWxEbtIM
-G4Op7vvNF1fQ6Ww9NTCwQosRfub121XZUBnCEN/7fSIC2Ypz1zWpkCPBL3Ly6mIT
-uX1urElaiBAigqy19aYg5balOFyl3/aw0vUsxG9caetpVp1eKYfVRk8eOPFJ69AS
-KwIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'wonderful.software': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3r3dehkLMOggLWshm6NS
-g2Ct1gtPsi618BaBwPa459xcCvDM9e0wiGujM1kkvyLtEligkoqnOoYZL2YdHfK5
-aR9hBu/3C/uIqa6LMPLP2FO4sOiGaBIIKvxGWvkZ9CzNN/dcBgqSAoMU4UfNd/vH
-NHqJaQ+euTVLPe5JwMtSJqxemREu1wuhAhGI3V8tb0JmYthcvLzpvxeJg1Hwyd1g
-BpU1AatXp1OivGXdHQF4vP69Szn775ZofQzqlXaboCDjJ1Zu9wuvxCzJlplml7C3
-NAFPAKjVtw86bZyH8rMK/mfmh+5VK5gY/QC55ZqV+iA2VazAm/PYb5k9/h3SR27D
-FwIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'webring.wonderful.software': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArCe1g28aEqSrRkucr9/P
-uspJog/kFyGYLQJc2m4FjmWAvnpsGrUJYdzqLEodhAFf6Lpi9M15t4t4kN1ko45o
-VEN+DOdgHp8c3EO76PeScWmg3Y5KS7JUsRY55cNG4mO/rDGwbCTaSA6L1XLz+hSU
-6vefp+y66G5RdS57FnNB9cfsmr2Xc8QxqeSV4ehOkgqEMPc1EYtZnpH1ZLYJa7OP
-zdfGM1Q/FbPhdJpEEYnCJGs0P9+Jkz85Vh12z3AP4WEgjD+v0LDgxLgvALRn1iUp
-CZEQZ9EZ83YnTxQhCyMd7ZQXKcx1kQFkmUPX7FPEMTfJunH5JHywHCXkGgfA6F7v
-4QIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'dtinth-webshots': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvgBT7qY48PCtoSILZ9Yq
-48ipyguM14bJqazGaZDYzY+FoXsFXfj/6v/FtuWn0gSiEKZ7Cd+9w256JA7rxCdZ
-ahe9CDYaaHPdglKSg93ncLaLal6KfNzCEaGvCeaKlt7cDNrNkKGg2chZaHKQbV26
-QWzZlSb8xUmNy3AwFd0/H81krCvolRu9zn7UNtXEeDKp7Gmxkr6kWFmWJimt53yg
-ogEnr+5IcxnbMKRn84DrxcFjf3NfjZWZ0lZUXMKHPVyW/ZFGC7lhOWNqwbfyun6B
-JX/9uRg4s1OysojxEks1k4HL5b55n+wnNSXGY+BUe5sum5o8LoEOq74K74cE1mKA
-NwIDAQAB
------END PUBLIC KEY-----`,
+    publicKey: `-----BEGIN RSA PUBLIC KEY-----
+MIIBigKCAYEA1+EelYd/Q9bvgidaMEP/CHIBGIDTdbMpM4OFD8ab/2gg5sRiW14t
+H0UKhDn1KRcG+Gd2zuZUZ+4ghOtIHHXA7rG01fuQcqUudBIyQLZt/amhWThcPNtN
+lRV5Xkc+tHHLFYbqYv+QVPmPg1GTq/Uj/rIdmSiv22uMUKQ+xuDQo02k0OwtGG5E
+QIoipO/gcugEAUS76S4usBcz7wGrkmH+mneYCOGZZzhJVbxggVvNDmRj9aHKemWM
++BtoyN71xeq3sX+6bMFWZQgm2qAE5BebjWoFBgyNMS7J6x8Ad8I2TLUXPiQOCfDi
+HGKRxsFBWb/14U4fkg78xfN2oS9l2R1WR8H08d6lo6Mul6xybQwZDkGIop8V0O4x
+o7eL51kNuNAw/2eoYByOnAxgS7hhfddzn2bTsxWiWwgdvGvR9Ar3tshQjZ5vN+f5
+IJnigKVopmlhb4Rn7erXt7UyDS4ZmsXV1NzauzPl3OfrIlgNeM8UMD0Qo6kYrVTD
+ky6jTrhvpRnfAgMBAAE=
+-----END RSA PUBLIC KEY-----`,
   },
 }
 
